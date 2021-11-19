827238619a4eb10449e60120e7751b83825e14eb
Anthony Viola <48599775+ajoeviola@users.noreply.github.com>
2021-11-18 20:20:11 -0500

    Added variables/getters and setters

--- Board.java
+++ Board.java
@@ -4,6 +4,23 @@ public class Board {
 	/**
 	 * variables: Tiles[][] (2d array to represent the board)
 	 */
+	int Tiles[][];
 	
-	//getters and setters (again, build in way of creating this!)***
+	
+	/**
+	 * Our default constructor
+	 */
+	public Board() {}
+
+	
+	//Getters and setters for Tiles[][]
+	public int[][] getTiles() {
+		return Tiles;
+	}
+
+	public void setTiles(int[][] tiles) {
+		Tiles = tiles;
+	}
+	
+
 }
--- Ship.java
+++ Ship.java
@@ -9,19 +9,72 @@ public class Ship {
 	 *  sunk: boolean (contains whether or not the ship has sunk
 	 *  name: String (Name of the type of ship)
 	 */
+	int length;
+	int width;
+	String orientation;
+	boolean sunk;
+	String name;
+	
 	
 	/**
 	 * sink is called once the ship is sunk, it changes the 'sunk' variable to true, and displays a message/visual
 	 */
-	public void sink() {
-		
-	}
+	public void sink() {}
+
 	
 	/**
-	 * 
+	 * Create getters and setters for class ship
+	 * @return
 	 */
+	public int getLength() {
+		return length;
+	}
+
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+
+	public int getWidth() {
+		return width;
+	}
+
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+
+	public String getOrientation() {
+		return orientation;
+	}
+
+
+	public void setOrientation(String orientation) {
+		this.orientation = orientation;
+	}
+
+
+	public boolean isSunk() {
+		return sunk;
+	}
+
+
+	public void setSunk(boolean sunk) {
+		this.sunk = sunk;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
-	//there is a command to add getters and setters for this class ****
 	
 	
 	
